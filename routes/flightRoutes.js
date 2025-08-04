6f5960125abfef9db3fe001d6815ad9836a701b3
Tirthraj-Raval <tushartirthraj@gmail.com>
2025-08-04 13:45:56 +0530

    Updated ROute for getting flight Status

--- routes/flightRoutes.js
+++ routes/flightRoutes.js
@@ -3,7 +3,7 @@ const flightController = require("../controllers/flightControlller");
 const router = express.Router();
 
 router.post('/:shipment_number/flights/add', flightController.addFlight);
-router.put('/flights/:flightNumber/status', flightController.getFlightStatus);
+router.put('/:flightNumber/status', flightController.getFlightStatus);
 
 module.exports = router;
 
